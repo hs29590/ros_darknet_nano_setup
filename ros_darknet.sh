b11ba1aba0034f6c310bc5d5868f5c0c4a1f8ea3
Harshit Sureka <hs29590@gmail.com>
2021-05-25 13:49:08 +0530

    Update ros_darknet.sh

--- ros_darknet.sh
+++ ros_darknet.sh
@@ -3,14 +3,13 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
 sudo apt update
-sudo apt install ros-melodic-desktop-full
+sudo apt install ros-melodic-desktop
 
 echo "export PATH=/usr/local/cuda-10.2/bin\${PATH:+:\${PATH}}" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}" >> ~/.bashrc
 
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
-source ~/.bashrc
 sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo apt install python-rosdep
 cd
@@ -18,23 +17,23 @@ mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/
 catkin_make
 cd
-sudo apt-get install build-essential git libopencv-dev
+source ~/.bashrc
+sudo apt-get install git
 git clone https://github.com/AlexeyAB/darknet.git
-git clone https://github.com/robro-ayush/darknet-makefile
+git clone https://github.com/hs29590/darknet-makefile
 cp darknet-makefile/Makefile darknet/
 cd darknet
 make
 sudo cp libdarknet.so /usr/local/lib/
 sudo cp include/darknet.h /usr/local/include/
 sudo ldconfig
-sudo apt-get install cmake build-essential libtclap-dev libmagic-dev libopencv-dev
+sudo apt-get install cmake libtclap-dev libmagic-dev
 cd
 git clone https://github.com/stephanecharette/DarkHelp.git
 cd DarkHelp
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
-make
 make package
 sudo dpkg -i darkhelp-*.deb
 #clone robro-cloud cosmetic-counting robro-socket
