6d3c30c056ebcf4abc58b74610a3c8f9486eb16f
Harshit Sureka <hs29590@gmail.com>
2021-09-18 13:42:36 +0530

    Update ros_darknet.sh

--- ros_darknet.sh
+++ ros_darknet.sh
@@ -5,11 +5,6 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
 sudo apt update
-# install if ROS, rqt, rviz, robot-generic libraries, 2D/3D simulators and 2D/3D perception needed
-sudo apt install ros-melodic-desktop-full
-# install if ROS, rqt, rviz, and robot-generic libraries
-sudo apt install ros-melodic-desktop
-# install if ROS package, build, and communication libraries. No GUI tools. use this for kiara
 sudo apt install ros-melodic-ros-base
 
 echo "export PATH=/usr/local/cuda-10.2/bin\${PATH:+:\${PATH}}" >> ~/.bashrc
